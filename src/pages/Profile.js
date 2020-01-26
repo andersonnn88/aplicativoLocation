41f62f7619d4bf06a27ea02c53e9c24df35624e2
Anderson araujo <andersonga4@gmail.com>
2020-01-26 18:56:01 -0200

    versao completa do app'

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {View} from 'react-native';
+import { WebView } from 'react-native-webview';
 
-function Profile(){
-    return <View/>
+function Profile({ navigation }){
+    const gitubUsername = navigation.getParam('github_username');
+    return <WebView style={{ flex:1 }}  source={{ uri: `https://github.com/${gitubUsername}`}}/>
 }
 export default Profile;
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,29 @@ import { createStackNavigator } from 'react-navigation-stack';
 import Main from './pages/Main';
 import Profile from './pages/Profile';
 
+
 const Routes = createAppContainer(
     createStackNavigator({
-        Main,
-        Profile,
-    }
-    )
+        Main :{
+            screen: Main,
+            navigationOptions: {
+                title: 'DevRadar'
+            },
+        },
+        Profile: {
+            screen: Profile,
+            navigationOptions:{
+                title: 'Perfil Github'
+            },
+        },
+    },{
+        defaultNavigationOptions: {
+            headerTintColor: '#FFF',
+            headerStyle:{
+                backgroundColor:'#7D40E7'
+            },
+        }
+    })
 )
 
 export default Routes;
